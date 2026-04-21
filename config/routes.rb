964c1e1768dc4182b95fe68c1af2b92a0415e898
RobertOltean314 <olteanrobertadrian314@gmail.com>
2026-04-21 14:25:54 +0300

    Add routes and layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'application#hello'
+  root 'static_pages#home'
+  get '/signup', to: 'user#new'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
 end
