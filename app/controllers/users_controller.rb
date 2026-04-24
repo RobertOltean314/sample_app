82e1b0c3a2cd507115ba4c2897fbef511d965ff9
RobertOltean314 <olteanrobertadrian314@gmail.com>
2026-04-24 13:17:31 +0300

    Add validation for name and email

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# This class represents the User controller
+class UsersController < ApplicationController
+  def new; end
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'Example User', email: 'example@gmail.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'should have name' do
+    @user.name = ' '
+    assert_not @user.valid?
+  end
+
+  test 'should have email' do
+    @user.email = ' '
+    assert_not @user.valid?
+  end
+
+  test 'name should not be to long' do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'email should not be to long' do
+    @user.email = 'a' * 151
+    assert_not @user.valid?
+  end
+
+  test 'email validation should acceppt only supported emails' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_USER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject unsupported email' do
+    invalid_addresses = %w[user@example,com USER@foo,COM A_USER@foo,bar,org first,last@foo,jp alice+bob@baz,cn]
+
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test 'email address should be unique' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+end
